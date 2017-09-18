4049c4bb9fd371e93ba18250ccd6b51e1c14a919
Gladysgong <531404376@qq.com>
2017-09-18 11:14:06 +0800

    url管理器

--- spiderbook/basicdistributedspider/URLManager.py
+++ spiderbook/basicdistributedspider/URLManager.py
@@ -1,7 +1,9 @@
 # coding:utf-8
 
+# import cPickle python2.7
 import _pickle as cPickle
 import hashlib
+
 
 # URL管理器
 class UrlManager(object):
@@ -16,7 +18,10 @@ class UrlManager(object):
     def get_new_url(self):
         '''获取一个未爬取的URL'''
         new_url = self.new_urls.pop()
-        self.old_urls.add(new_url)
+        m = hashlib.md5()
+        m.update(new_url)
+        self.old_urls.add(m.hexdigest()[8:-8])
+        # self.old_urls.add(new_url)
         return new_url
 
     def add_new_url(self, url):
@@ -27,7 +32,11 @@ class UrlManager(object):
         '''
         if url is None:
             return
-        if url not in self.new_urls and url not in self.old_urls:  # 去重
+        m = hashlib.md5()
+        m.update(url)
+        url_md5 = m.hexdigest()[8:-8]
+        # if url not in self.new_urls and url not in self.old_urls:  # 去重
+        if url not in self.new_urls and url_md5 not in self.old_urls:  # 去重
             self.new_urls.add(url)
 
     def add_new_urls(self, urls):
@@ -54,3 +63,28 @@ class UrlManager(object):
         :return:
         '''
         return len(self.old_urls)
+
+    def save_progress(self, path, data):
+        '''
+        保存进度
+        :param path:文件路径
+        :param data: 数据
+        :return:
+        '''
+        with open(path, 'wb') as f:
+            cPickle.dump(data, f)
+
+    def load_progress(self, path):
+        '''
+        从本地文件加载进度
+        :param paht: 文件路径
+        :return: 返回set集合
+        '''
+        print('[+]从文件加载进度:%s' % path)
+        try:
+            with open(path, 'rb') as f:
+                tmp = cPickle.load(f)
+                return tmp
+        except:
+            print('[!]无进度文件，创建:%s' % path)
+        return set()
